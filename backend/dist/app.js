1fe2ad2bdb8127b0c7a7e73cbf95eae84f456982
cody767426 <dhamiroshan730@gmail.com>
2024-09-29 18:10:26 +0530

    idk

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { config } from 'dotenv';
-import morgon from 'morgan';
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -14,7 +13,6 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 // remove it in production
-app.use(morgon("dev"));
 app.use("/api/v1", appRouter);
 export default app;
 //# sourceMappingURL=app.js.map
